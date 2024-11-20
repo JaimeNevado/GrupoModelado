3ee2400c9b2fef178851b0ba56129f8d867b5f05
Marta <martagranado1011@uma.es>
2024-11-20 09:09:12 +0100

    voluntario es el que hace la adopcion en vez de el adoptante

--- Práctica 3/Codigo/P3/src/Voluntario.java
+++ Práctica 3/Codigo/P3/src/Voluntario.java
@@ -16,6 +16,16 @@ public class Voluntario extends Socio {
     public void tramitarAdopcion(Animal a, Adoptante ad) {
         Adopcion adopcion = new Adopcion(new Date(), a, ad);
         adopciones.add(adopcion);   
+        a.setEstado(EstadoAnimal.adoptado);
+
+        Refugio refugio = this.getRefugio();
+        //no se si hace falta un if else o solo un
+        //refugio.getAnimalesRefugiados().remove(a)
+        if (refugio.getAnimalesRefugiados().remove(a)) {
+            System.out.println("El animal " + a.getNombre() + " ha sido adoptado y eliminado del refugio.");
+        } else {
+            System.out.println("El animal " + a.getNombre() + " no está en la lista de animales refugiados.");
+        }
     }
 
 	/**
